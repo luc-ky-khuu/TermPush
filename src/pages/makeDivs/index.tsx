abb92fd1725df3282025eff651f277d53e66c926
khuu96 <48267398+khuu96@users.noreply.github.com>
2023-01-29 15:57:25 -0800

    added tailwind

--- src/pages/makeDivs/index.tsx
+++ src/pages/makeDivs/index.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import CreateDiv from '../../components/makeDivs'
 import { IDivProps } from '../../interface/index.interface'
+import '../../app/globals.css'
 
 export default function addDiv(): JSX.Element {
 	let [divs, addDiv] = useState<Array<Object>>([]);
 	let [num, incNum] = useState<number>(0);
 	let [clickedEvent, setClickedEvent] = useState<React.MouseEvent<HTMLButtonElement>>(null)
-
 	useEffect(() => {
 		if (clickedEvent === null) return;
 		const childNodes: Array<HTMLElement> = clickedEvent.target.parentNode.childNodes;
 		const idx: number = Array.prototype.indexOf.call(childNodes, clickedEvent.target);
 		appendDivToIdx(idx)
+		incNum(num += 2);
 	}, [clickedEvent])
 	const props: IDivProps = {
 		id: num,
@@ -20,13 +21,21 @@ export default function addDiv(): JSX.Element {
 	}
 
 	const appendDivToBottom = () => {
-		addDiv([...divs, { idx: num, jsx: <CreateDiv key={num} props={ props } />}, { idx: num + 1, jsx: <button key={`${num}but`} onClick={(event) => insertDiv(event)}>Insert</button>} ])
+		addDiv([
+			...divs, 
+			{ idx: num, jsx: <CreateDiv key={num} props={ props } />}, 
+			{ idx: num + 1, jsx: <button className='' key={`${num}but`} onClick={(event) => insertDiv(event)}>Insert</button>}
+		])
 		incNum(num += 2);
 	}
 
 	const appendDivToIdx = (idx: number) => {
-		addDiv([...divs.slice(0, idx + 1), { idx: num + 1, jsx: <CreateDiv key={num + 1} props={ props } />}, { idx: num + 2, jsx: <button key={`${num + 2}but`} onClick={(event) => insertDiv(event)}>Insert</button>}, ...divs.slice(idx + 1)])
-		incNum(num += 2);
+		addDiv([
+			...divs.slice(0, idx + 1),
+			{ idx: num + 1, jsx: <CreateDiv key={num + 1} props={ props } />},
+			{ idx: num + 2, jsx: <button className='' key={`${num + 2}but`} onClick={(event) => insertDiv(event)}>Insert</button>},
+			...divs.slice(idx + 1)
+		])
 	}
 
 	const insertDiv = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -36,7 +45,7 @@ export default function addDiv(): JSX.Element {
 		<>
 			<button onClick={appendDivToBottom}>Add Div</button>
 			<div>
-				{divs.map((item: Object) => { return item.jsx })}
+				{divs.map((item: any) => { return item.jsx })}
 			</div>
 		</>
 	)
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./src/pages/_app.tsx",
+        "./src/pages/index.tsx",
+        "./src/pages/makeDivs/index.tsx",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+  }

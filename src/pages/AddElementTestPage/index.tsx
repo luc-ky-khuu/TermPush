00ac353cf4b3e58f668bfc8055b8dcd5edc9df48
khuu96 <48267398+khuu96@users.noreply.github.com>
2023-03-26 15:07:34 -0700

    issue-6 added function to add dom elements to page

--- src/pages/AddElementTestPage/index.tsx
+++ src/pages/AddElementTestPage/index.tsx
@@ -4,7 +4,7 @@ export default function addDiv(): JSX.Element {
 	return (
 		<>
 			<div id='mainContainer'key='testing1'>
-				<AddReactElement key='testing' element={{ element: 'div', children: [{ element: 'div', children: [], classes: [], text: 'test'}], classes: [], text: 'test'}}/>
+				<AddReactElement key='testing' element={{ element: 'div', children: [{ element: 'div', children: [], classes: [], text: 'test'}, { element: 'div', children: [], classes: [], text: 'test2'}], classes: [], text: 'test'}}/>
 			</div>
 		</>
 	)

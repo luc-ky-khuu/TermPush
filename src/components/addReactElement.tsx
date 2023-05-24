182904aa66234297134b9b49c5ee6faffd5e735f
khuu96 <48267398+khuu96@users.noreply.github.com>
2023-05-23 19:13:40 -0700

    issue-6 updated removeElement function

--- src/components/addReactElement.tsx
+++ src/components/addReactElement.tsx
@@ -9,7 +9,7 @@ import { pageElementObject } from '@/interface/AddElementTestPage.interface';
  */
 
 export default function AddReactElement({ ...props }): JSX.Element {
-    const { element, selected, onSelect, index } = props
+    const { element, selected, onSelect, removeParentElement } = props
     const ref = useRef(element.id);
 
 	let [pageElement, setPageElement] = useState<pageElementObject>(element);
@@ -47,24 +47,21 @@ export default function AddReactElement({ ...props }): JSX.Element {
             setIdCounter({...idCounter});
             setPageElement({...pageElement});
         },
-        removeElement: () => {
-            pageElement.parentRef.current.removeElementCall(index);
-        },
-        removeElementCall: (index) => {
-            pageElement.children.splice(index, 1);
-            setPageElement({...pageElement});
-        }
     }))
+
+    const removeElement = (index: number) => {
+        pageElement.children.splice(index, 1);
+        setPageElement({...pageElement});
+    };
 
     const renderPageElements = (pageElementObject: pageElementObject) => {
         const { type, children, classes, text } = pageElementObject; //id here is the child id
-        // these are just buttons to test out adding, will be removed later in final product.
         //added text as first item in array.
         const reactChildren: Array<any>= [text];
         //if there are any nested (children) elements, render recursively
         if (children.length > 0) {
             for (let i = 0; i < children.length; i++) {
-                reactChildren.push(<AddReactElement selected={selected} onSelect={onSelect} element={children[i]} key={children[i].id} index={i}/>);
+                reactChildren.push(<AddReactElement selected={selected} onSelect={onSelect} element={children[i]} key={children[i].id} removeParentElement={() => removeElement(i)}/>);
             }
         };
         //add buttons for ability to keep adding, will be removed later in final product.		
@@ -79,12 +76,11 @@ export default function AddReactElement({ ...props }): JSX.Element {
                 onClick: (event) => {
                     event.preventDefault();
                     event.stopPropagation();
-                    const parentId = event.target.parentElement.id;
-                    onSelect(event.target.id, ref, parentId);
+                    onSelect(event.target.id, ref);
                     
                 }
             },
-             reactChildren);
+             [<button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded' onClick={removeParentElement}>remove</button>, ...reactChildren]);
     };
 
 	return (
